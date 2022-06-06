b534c1dd88785b222be479f1ec27dd1ff39a6979
giulio <notice@carburo.net>
2022-06-06 15:10:22 +0200

    landing live

--- public/site/templates/landing.php
+++ public/site/templates/landing.php
@@ -1,141 +1,137 @@
 <?php require 'inc/head.php' ?>
   <body class="" >
-	<!-- Container div -->
-		<!-- Navigation -->
-		<div class="flex bg-white h-15p mt-auto px-16 sticky top-0 z-50">
-					<div class="uppercase my-auto text-3xl tracking-widest">
-						<a href="<?= $homepage->url ?>">
-							<span class="font-bold ">Vimar</span><span class="font-light">1991</span>
-						</a>
-					</div>
-		</div>
+  	<!-- Container div -->
+  		<!-- Navigation -->
+  		<div class="flex bg-white h-15p mt-auto px-16 sticky top-0 z-50">
+  					<div class="uppercase my-auto  tracking-widest">
+  						<a class="text-5xl" href="<?= $homepage->url ?>">
+  							<span class="font-semibold ">Vimar</span><span class="font-light">1991</span>
+  						</a>
+  					</div>
+  		</div>
 
-		<!-- Main container -->
-		<div class="pt-7 pb-15 px-16 text-black">
+  		<!-- Main container -->
+  		<div class="pt-7 pb-15 px-16 text-black">
 
-			<!-- Title -->
-			<div class="text-5xl font-light py-15 w-52 tracking-widest">
-				<h1>INSIDE LOOKING OUT</h1>
-			</div>
-
-
-			<!-- Strillo destra -->
-			<div class="text-3xl leading-7 font-bold w-1/2 tracking-widest ml-auto pb-15">
-				<h1 class=" w-4/5">
-				MILANO DESIGN WEEK
-				7/12 GIU. 22
-				</h1>
-			</div>
-			
-			<!-- Strillo sinistra -->
-			<div class="text-3xl leading-7 font-bold w-7/12 tracking-widest mr-auto ">
-				<h2>
-					VIMAR1991 È PARTNER TECNICO DI INSIDE LOOKING OUT, A MULTI–SENSORY APARTMENT INSTALLATION BY ELISA OSSINO STUDIO. 
-				</h2>
-			</div>
-
-			<!-- immagine destra -->
-			<div class="float-right w-1/2 py-12">
-				<img src="<?php echo $config->urls->templates?>landing/elisa-ossino_cut.jpg" alt="Elisa Ossino Studio">
-			</div>
-			<span class="block clear-both"></span>
-
-			<!-- Strillo sinistra -->
-			<div class="text-3xl leading-7 font-bold w-7/12 tracking-widest mr-auto ">
-				<p>
-					IN COLLABORAZIONE CON LA DESIGNER ELISA OSSINO, VIMAR1991 PRESENTERÀ DURANTE LA DESIGN WEEK, UN TESSUTO PER IMBOTTITI DAI TONI NATURALI E CALDI IN GRADO DI VALORIZZARE LA PREZIOSITÀ DELLA FIBRA 
-					E LE FORME CHE VESTIRÀ. 
-					IL TESSUTO, LAVORATO A MANO, ESALTA LA PUREZZA NATURALE DELLA LANA IN UN’OTTICA DI SOSTENIBILITÀ 
-					E VALORIZZAZIONE DEL 
-					KNOW HOW ITALIANO. 
-				</p>
-			</div>
-
-			<!-- Banner picture -->
-			<div class="w-full pt-24 pb-20">
-				<img class="w-full" src="<?= $config->urls->templates?>landing/FULL.jpg" alt="tessuto lana">
-			</div>
+  			<!-- Title -->
+  			<div class="text-5xl font-light py-15 w-52 tracking-widest">
+  				<h1>INSIDE LOOKING OUT</h1>
+  			</div>
 
 
-		
+  			<!-- Strillo destra -->
+  			<div class="text-3xl leading-7 font-semibold w-full md:w-1/2 tracking-widest ml-auto pb-15">
+  				<h1 class=" w-4/5">
+  				MILANO DESIGN WEEK
+  				7/12 GIU. 22
+  				</h1>
+  			</div>
+  			
+  			<!-- Strillo sinistra -->
+  			<div class="text-3xl leading-7 font-semibold w-full md:w-7/12 tracking-widest mr-auto ">
+  				<h2>
+  					VIMAR1991 È PARTNER TECNICO DI INSIDE LOOKING OUT, A MULTI–SENSORY APARTMENT INSTALLATION BY ELISA OSSINO STUDIO. 
+  				</h2>
+  			</div>
 
-			<!-- Right text -->
-			<div class="text-base leading-5 tracking-widest w-1/2 ml-auto pb-15">
-				<p class="pr-10">
-				Questa tipologia di filati nuovi per l’arredamento, sono 
-				il frutto di lavorazioni specifiche della filatura fantasia. 
-				Il bouclè, infatti, è lavorato per creare le boccole, 
-				mentre la pelliccia nasce come lineare e poi viene ritorta per creare un filato effetto ciniglia. Anche il finissaggio usato per i filati sposa la poetica materica della designer, trattandosi di un vaporizzo in matasse, 
-				che favorisce il rigonfiamento della lana e restituisce volume e rotondità al filato senza andare ad intaccare 
-				la bellezza della fibra di cui è composto. 
-				</p>
-			</div>
+  			<!-- immagine destra -->
+  			<div class="md:float-right md:w-1/2 py-12">
+  				<img src="../landing/elisa-ossino_cut.jpg" alt="Elisa Ossino Studio">
+  			</div>
+  			<span class="block clear-both"></span>
 
+  			<!-- Strillo sinistra -->
+  			<div class="text-3xl leading-7 font-semibold md:w-7/12 tracking-widest mr-auto ">
+  				<p>
+  					IN COLLABORAZIONE CON LA DESIGNER ELISA OSSINO, VIMAR1991 PRESENTERÀ DURANTE LA DESIGN WEEK, UN TESSUTO PER IMBOTTITI DAI TONI NATURALI E CALDI IN GRADO DI VALORIZZARE LA PREZIOSITÀ DELLA FIBRA 
+  					E LE FORME CHE VESTIRÀ. 
+  					IL TESSUTO, LAVORATO A MANO, ESALTA LA PUREZZA NATURALE DELLA LANA IN UN’OTTICA DI SOSTENIBILITÀ 
+  					E VALORIZZAZIONE DEL 
+  					KNOW HOW ITALIANO. 
+  				</p>
+  			</div>
 
-			<!-- Strillo sinistra -->
-			<div class="text-3xl leading-7 font-bold w-7/12 tracking-widest mr-auto ">
-				<p>
-					LO STILE E L’ELEGANZA GUIDANO DA SEMPRE IL CICLO PRODUTTIVO DELL’AZIENDA, RENDENDONE I PRODOTTI RICONOSCIBILI, 
-					UNICI E RAFFINATI.
-					AD ACCOMPAGNARE QUESTA RICERCA, VI SONO UNA CONTINUA INNOVAZIONE E SPERIMENTAZIONE NELL’UTILIZZO DI FIBRE E MATERIALI INSIEME AD UN KNOW HOW ACQUISITO NEGLI ANNI MA RIPROPOSTO SEMPRE IN CHIAVE CONTEMPORANEA.
-				</p>
-			</div>
-
-			<!-- One picture -->
-			<div class="w-700px mx-auto py-15">
-				<img src="<?= $config->urls->templates?>landing/750x505_02.jpg" alt="filati">
-			</div>
-
-			<!-- Small text right -->
-			<div class="text-sm leading-4 tracking-widest w-1/2 ml-auto pb-15">
-				<p class="pb-6 pr-10">
-					Da qui nasce l’eccezionale varietà dei filati che Vimar 
-					è in grado di produrre e presentare al mondo, facendosi promotrice di un’instancabile voglia di novità ed esclusività.
-				</p>
-				<p class="pb-6 pr-10">
-					Da giugno 2020 l’azienda ha avviato un rinnovamento interno, che ha come cardini la tracciabilità e la sostenibilità dei materiali utilizzati, oltre all’upcycling degli scarti di produzione. 
-					Oggi è un’azienda certificata in grado di offrire prodotti che incarnino una responsabilità etica produttiva oltre ad un’eccellenza di stile e ricercatezza.
-				</p>
-				<p class="pb-6 pr-10">
-					Lo stile e l’eleganza guidano da sempre il ciclo produttivo dell’azienda, rendendone i prodotti riconoscibili, unici e raffinati.
-					Ad accompagnare questa ricerca, vi sono una continua innovazione e sperimentazione nell’utilizzo di fibre e materiali insieme ad un know how acquisito negli anni ma riproposto sempre in chiave contemporanea. 
-					Da qui nasce l’eccezionale varietà dei filati che Vimar è in grado di produrre e presentare al mondo, 
-					facendosi promotrice di un’instancabile voglia di novità ed esclusività.
-					Da giugno 2020 l’azienda ha avviato un rinnovamento interno, che ha come cardini la tracciabilità e la sostenibilità dei materiali utilizzati, oltre all’upcycling degli scarti di produzione. 
-					Oggi è un’azienda certificata in grado di offrire prodotti che incarnino una responsabilità etica produttiva oltre ad un’eccellenza di stile e ricercatezza.
-				</p>
-			</div>
+  			<!-- Banner picture -->
+  			<div class="w-full pt-24 pb-20">
+  				<img class="w-full" src="../landing/FULL.jpg" alt="tessuto lana">
+  			</div>
 
 
-		</div>
-
-  	<!-- footer -->
-  	<footer class="mt-12 px-16 ">
   		
-  	<p class="text-3xl leading-7 font-bold w-1/2 tracking-widest pt-7">
-	 	INSIDE<br>
-  	LOOKING<br>
-  	OUT<br>
-  	</p>
 
-  	<p class="text-3xl leading-7 font-bold w-1/2 tracking-widest pt-7">
-  	MILANO DESIGN WEEK<br>
-  	7/12 GIU. 22<br>
-  	</p>
+  			<!-- Right text -->
+  			<div class="text-base leading-5 tracking-widest md:w-1/2 ml-auto pb-15">
+  				<p class="pr-10">
+  				I filati scelti esaltano la matericità dei tessuti  proponendo una texture elegante ed irregolare in grado di restituire profondità tattile e visiva all’elemento.
+  				Un boucle e una pelliccia che nel seguire la plasticità della forma celebrano anche la bellezza della fibra naturale utilizzata.
+  				</p>
+  			</div>
 
-  	<p class="text-3xl leading-7 font-bold w-1/2 tracking-widest pt-7">
-  	VIA SOLFERI NO 11<br>
-  	TERZO PIANO<br>
-  	</p>
 
-  	<!-- Small text right -->
-  	<div class=" w-1/2 ml-auto py-15">
-  		<p class="text-s1 leading-4 tracking-widest">
-			VIMAR 1991 srl <span class="pl-3">via per Santhià 8</span> <span class="pl-3">Carisio (VC)</span>  <span class="pl-3">13040 Italia</span>
-  		</p>
-  	</div>
+  			<!-- Strillo sinistra -->
+  			<div class="text-3xl leading-7 font-semibold md:w-7/12 tracking-widest mr-auto ">
+  				<p>
+  					LO STILE E L’ELEGANZA GUIDANO DA SEMPRE IL CICLO PRODUTTIVO DELL’AZIENDA, RENDENDONE I PRODOTTI RICONOSCIBILI, 
+  					UNICI E RAFFINATI.
+  					AD ACCOMPAGNARE QUESTA RICERCA, VI SONO UNA CONTINUA INNOVAZIONE E SPERIMENTAZIONE NELL’UTILIZZO DI FIBRE E MATERIALI INSIEME AD UN KNOW HOW ACQUISITO NEGLI ANNI MA RIPROPOSTO SEMPRE IN CHIAVE CONTEMPORANEA.
+  				</p>
+  			</div>
 
-  	</footer>
-</body>
+  			<!-- One picture -->
+  			<div class="w-700px mx-auto py-15">
+  				<img src="../landing/750x505_02.jpg" alt="filati">
+  			</div>
+
+  			<!-- Small text right -->
+  			<div class="text-sm leading-4 tracking-widest md:w-1/2 ml-auto pb-15">
+  				<p class="pb-6 pr-10">
+  					Da qui nasce l’eccezionale varietà dei filati che Vimar
+  					è in grado di produrre e presentare al mondo, facendosi
+  					promotrice di un’instancabile voglia di novità ed esclusività.
+  					Da giugno 2020 l ’azienda ha avviato un rinnovamento
+  					interno, che ha come cardini la tracciabilità e la sostenibilità
+  					dei materiali utilizzati, oltre all ’upcycling degli scarti di
+  					produzione.
+  				</p>
+
+  				<p class="pb-6 pr-10">
+  					Oggi è un’azienda certificata in grado di offrire prodotti
+  					che incarnino una responsabilità etica produttiva oltre ad
+  					un’eccellenza di stile e ricercatezza.
+  				</p>
+
+  			</div>
+
+
+  		</div>
+
+    	<!-- footer -->
+    	<footer class="mt-12 px-16 ">
+    		
+    	<p class="text-3xl leading-7 font-semibold w-1/2 tracking-widest">
+  	 	INSIDE<br>
+    	LOOKING<br>
+    	OUT<br>
+    	</p>
+
+    	<p class="text-3xl leading-7 font-semibold w-1/2 tracking-widest pt-7">
+    	MILANO DESIGN WEEK<br>
+    	7/12 GIU. 22<br>
+    	</p>
+
+    	<p class="text-3xl leading-7 font-semibold w-1/2 tracking-widest pt-7">
+    	VIA SOLFERI NO 11<br>
+    	TERZO PIANO<br>
+    	</p>
+
+    	<!-- Small text right -->
+    	<div class=" w-1/2 ml-auto py-15">
+    		<p class="text-s1 leading-4 tracking-widest">
+  			VIMAR 1991 srl <span class="pl-3">via per Santhià 8</span> <span class="pl-3">Carisio (VC)</span>  <span class="pl-3">13040 Italia</span>
+    		</p>
+    	</div>
+
+    	</footer>
+  </body>
 
 </html>
